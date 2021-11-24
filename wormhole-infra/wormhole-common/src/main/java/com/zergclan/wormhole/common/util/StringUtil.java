09237044863f4641094f9fa3997c6641444ca07f
WtlKoma <15810322721@163.com>
2021-11-24 15:23:34 +0800

    Add string tools.

---
+++ wormhole-infra/wormhole-common/src/main/java/com/zergclan/wormhole/common/util/StringUtil.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.zergclan.wormhole.common.util;
+
+/**
+ * String tools.
+ */
+public final class StringUtil {
+
+    /**
+     * Determine whether the string is empty.
+     * @param cs String.
+     * @return Boolean.
+     */
+    public static boolean isBlank(final CharSequence cs) {
+        int strLen;
+        if (cs != null && (strLen = cs.length()) != 0) {
+            for (int i = 0; i < strLen; ++i) {
+                if (!Character.isWhitespace(cs.charAt(i))) {
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    }
+
+    /**
+     * Determine whether the string is not empty.
+     * @param cs string.
+     * @return boolean.
+     */
+    public static boolean isNotBlank(final CharSequence cs) {
+        return !isBlank(cs);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isBlank(null));
+    }
+
+}
