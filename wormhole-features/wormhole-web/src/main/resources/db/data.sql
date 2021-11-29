463615134a01f6f789cb8ec49e3a2322ca0002d5
houyang <jacky7boy@163.com>
2021-11-29 19:40:25 +0800

    Optimize module structure of web to console.

--- wormhole-features/wormhole-web/src/main/resources/db/data.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM `user_info`;
-
-INSERT INTO `user_info` (id, `user_name`, `password`, `email`, `status`, `create_time`, `modify_time`) VALUES
-(1, 'jack', '123456', 'jacky7boy@163.com', 0, null, null),
-(2, 'rose', '123456', 'jacky7boy@163.com', 0, null, null);
