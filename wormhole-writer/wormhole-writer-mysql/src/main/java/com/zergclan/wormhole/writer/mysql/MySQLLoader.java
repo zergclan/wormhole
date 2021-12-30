15adf03d003546eeedc992090db75b38904edfbe
wenguozhang <1138254567@qq.com>
2021-12-30 14:11:36 +0800

    Mysql loader (#57)
    * mysql write data
    
    * mysql write data

--- wormhole-writer/wormhole-writer-mysql/src/main/java/com/zergclan/wormhole/writer/mysql/MySQLLoader.java
+++ wormhole-writer/wormhole-writer-mysql/src/main/java/com/zergclan/wormhole/writer/mysql/MySQLLoader.java
@@ -18,16 +18,74 @@
 package com.zergclan.wormhole.writer.mysql;
 
 import com.zergclan.wormhole.loader.Loader;
+import lombok.Data;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Map;
 
 /**
  * Loader for MySQL.
  */
+@Data
 public class MySQLLoader implements Loader {
+
+    private JdbcTemplate jdbcTemplate;
+
+    private String targetTable;
+
+    private String selectSql;
+
+    private String insertSql;
+
+    private String updateSql;
 
     @Override
     public void loaderData(final Map<String, Object> map) {
+        if ("target_table".equals(targetTable)) {
+            try {
+                Connection connection = jdbcTemplate.getDataSource().getConnection();
+                //select
+                PreparedStatement ps = connection.prepareStatement(selectSql);
+                ps.setLong(1, Long.valueOf(String.valueOf(map.get("transBigint"))));
+                ps.setString(2, String.valueOf(map.get("transVarchar")));
+                System.out.println(ps.toString());
+                ResultSet rs = ps.executeQuery();
+
+                //insert or update
+                if (rs.getRow() == 0) {
+                    System.out.println("insert...");
+                    PreparedStatement ps1 = connection.prepareStatement(insertSql);
+                    ps1.setInt(1, Integer.valueOf(String.valueOf(map.get("transInt"))));
+                    ps1.setLong(2, Long.valueOf(String.valueOf(map.get("transBigint"))));
+                    ps1.setString(3, String.valueOf(map.get("transVarchar")));
+                    ps1.setBigDecimal(4, new BigDecimal(String.valueOf(map.get("transDecimal"))));
+                    ps1.setObject(5, map.get("transDatetime"));
+                    System.out.println(ps1.toString());
+                    ps1.execute();
+                } else {
+                    System.out.println("update...");
+                    PreparedStatement ps2 = connection.prepareStatement(updateSql);
+                    ps2.setInt(1, Integer.valueOf(String.valueOf(map.get("transInt"))));
+                    ps2.setBigDecimal(2, new BigDecimal(String.valueOf(map.get("transDecimal"))));
+                    ps2.setObject(3, map.get("transDatetime"));
+                    ps2.setLong(4, Long.valueOf(String.valueOf(map.get("transBigint"))));
+                    ps2.setString(5, String.valueOf(map.get("transVarchar")));
+                    System.out.println(ps2.toString());
+                    ps2.execute();
+                }
+            } catch (SQLException throwables) {
+                System.out.println("error...");
+                throwables.printStackTrace();
+            }
+        } else {
+            // TODO the relationship between tables and data
+            System.out.println("no implement...");
+        }
 
     }
 }
