8a0d1d623fae4a63425d0956b4583418134556d8
gin <jacky7boy@163.com>
2022-01-13 11:45:11 +0800

    add cors configuration source. (#77)

--- wormhole-console/src/main/java/com/zergclan/wormhole/console/api/security/SecurityConfiguration.java
+++ wormhole-console/src/main/java/com/zergclan/wormhole/console/api/security/SecurityConfiguration.java
@@ -17,14 +17,20 @@
 
 package com.zergclan.wormhole.console.api.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -41,5 +47,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/security/login").permitAll().anyRequest().authenticated()
                 .and().exceptionHandling().authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage()))
                 .and().csrf().disable();
+    }
+    
+    /**
+     * Config {@link CorsConfigurationSource}.
+     *
+     * @return {@link CorsConfigurationSource}
+     */
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Collections.singletonList(CorsConfiguration.ALL));
+        corsConfiguration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE"));
+        corsConfiguration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource result = new UrlBasedCorsConfigurationSource();
+        result.registerCorsConfiguration("/**", corsConfiguration);
+        return result;
     }
 }
