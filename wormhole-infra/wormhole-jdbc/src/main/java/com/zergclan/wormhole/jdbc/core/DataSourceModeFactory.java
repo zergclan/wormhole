d1b3fe374bb4a02e6dcda225cefcff27f2c9279b
wenguozhang <1138254567@qq.com>
2022-02-24 18:24:18 +0800

    use DatabaseMetaData

---
+++ wormhole-infra/wormhole-jdbc/src/main/java/com/zergclan/wormhole/jdbc/core/DataSourceModeFactory.java
@@ -0,0 +1,113 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.zergclan.wormhole.jdbc.core;
+
+import com.zergclan.wormhole.core.metadata.resource.DatabaseType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.util.UUID;
+
+/**
+ * create factory for data source by h2.
+ */
+public final class DataSourceModeFactory {
+
+    /**
+     * get data source by mode.
+     * @param  mode {@link DatabaseType}
+     * @return DataSource
+     */
+    public DataSource getDataSource(final DatabaseType mode) {
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        String url = "jdbc:h2:file:~/.h2/" + uuid + ";AUTO_SERVER=TRUE;MODE=" + mode.getName();
+        System.out.println(url);
+        dataSource.setUrl(url);
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        return dataSource;
+    }
+
+    /**
+     * get test data source by mode.
+     * @param  mode {@link DatabaseType}
+     * @return DataSource
+     */
+    public DataSource getTestDataSource(final DatabaseType mode) {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        String url = "jdbc:h2:file:~/.h2/test;AUTO_SERVER=TRUE;MODE=" + mode.getName();
+        System.out.println(url);
+        dataSource.setUrl(url);
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        return dataSource;
+    }
+
+    /**
+     * get a data source by mode to be the origin of sync data.
+     * @return DataSource
+     */
+    public DataSource getOriginDataSource() {
+        DataSource dataSource = getDataSource(DatabaseType.MYSQL);
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        String createUserTableSqlStr = "CREATE TABLE `user_info` (\n"
+                + "    `id` INT(11) AUTO_INCREMENT COMMENT '主键',\n"
+                + "    `username` VARCHAR(64) NOT NULL COMMENT '用户名',\n"
+                + "    `password` VARCHAR(64) NOT NULL COMMENT '用户密码',\n"
+                + "    `email` VARCHAR(64) NOT NULL COMMENT '用户邮箱',\n"
+                + "    `is_enable` tinyint(4) NOT NULL COMMENT '0未激活，1激活',\n"
+                + "    `create_time` datetime(0) NOT NULL COMMENT '创建时间',\n"
+                + "    `modify_time` datetime(0) NOT NULL COMMENT '更新时间',\n"
+                + "    PRIMARY KEY (`id`)\n"
+                + ") ENGINE = InnoDB CHARACTER SET = utf8 COMMENT = '用户信息表';";
+        jdbcTemplate.execute(createUserTableSqlStr);
+        String insertUserTableSqlStr = "INSERT INTO `user_info`(`id`, `username`, `password`, `email`, `is_enable`, `create_time`, `modify_time`) "
+                + "VALUES (1, 'h2', 'h2', 'test', 1, '2021-12-15 17:38:57', '2021-12-15 17:39:01');";
+        jdbcTemplate.execute(insertUserTableSqlStr);
+
+        return dataSource;
+    }
+
+    /**
+     * get a data source by mode to be the target of sync data.
+     * @return DataSource
+     */
+    public DataSource getTargetDataSource() {
+        DataSource dataSource = getDataSource(DatabaseType.MYSQL);
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        String createUserTableSqlStr = "CREATE TABLE `user_info` (\n"
+                + "    `id` INT(11) AUTO_INCREMENT COMMENT '主键',\n"
+                + "    `username` VARCHAR(64) NOT NULL COMMENT '用户名',\n"
+                + "    `password` VARCHAR(64) NOT NULL COMMENT '用户密码',\n"
+                + "    `email` VARCHAR(64) NOT NULL COMMENT '用户邮箱',\n"
+                + "    `is_enable` tinyint(4) NOT NULL COMMENT '0未激活，1激活',\n"
+                + "    `create_time` datetime(0) NOT NULL COMMENT '创建时间',\n"
+                + "    `modify_time` datetime(0) NOT NULL COMMENT '更新时间',\n"
+                + "    PRIMARY KEY (`id`)\n"
+                + ") ENGINE = InnoDB CHARACTER SET = utf8 COMMENT = '用户信息表';";
+        jdbcTemplate.execute(createUserTableSqlStr);
+
+        return dataSource;
+    }
+}
---
+++ wormhole-infra/wormhole-jdbc/src/main/java/com/zergclan/wormhole/jdbc/core/MySQLMetaDataHandler.java
@@ -0,0 +1,214 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.zergclan.wormhole.jdbc.core;
+
+import com.zergclan.wormhole.core.metadata.resource.ColumnMetadata;
+import com.zergclan.wormhole.core.metadata.resource.IndexMetadata;
+import com.zergclan.wormhole.core.metadata.resource.TableMetadata;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.LinkedList;
+
+/**
+ * mysql metadata.
+ */
+public class MySQLMetaDataHandler {
+
+    /**
+     * get table by DatabaseMetaData.
+     * @param connection {@link Connection}
+     * @param schema {@link String}
+     * @return Collection
+     * @throws SQLException notNull
+     */
+    public static Collection<TableMetadata> getTables(final Connection connection, final String schema) throws SQLException {
+        Map<String, TableMetadata> tableMap = new HashMap<>(16);
+        DatabaseMetaData databaseMetaData = getDatabaseMetaData(connection);
+
+        List<Map<String, String>> columnsList = initColumns(databaseMetaData, null, schema, null, null);
+        for (Map<String, String> map : columnsList) {
+            TableMetadata tableMetadata = tableMap.get(map.get("TABLE_NAME"));
+            if (null == tableMetadata) {
+                tableMetadata = new TableMetadata(map.get("TABLE_NAME"), schema, map.get("TABLE_NAME"));
+                tableMap.put(map.get("TABLE_NAME"), tableMetadata);
+            }
+            ColumnMetadata columnMetadata = new ColumnMetadata(map.get("COLUMN_NAME"), schema, map.get("TABLE_NAME"),
+                    map.get("COLUMN_NAME"), map.get("TYPE_NAME"), "0".equals(map.get("NULLABLE")));
+            tableMetadata.registerColumn(columnMetadata);
+        }
+
+//        List<Map<String, String>> indexInfoList = initIndex(databaseMetaData, null, schema, null, false, false);
+//        for (Map<String, String> map : indexInfoList) {
+//            TableMetadata tableMetadata = tableMap.get(map.get("TABLE_NAME"));
+//            IndexMetadata indexMetadata = tableMetadata.getIndexes().get(map.get("INDEX_NAME"));
+//            if (null == indexMetadata) {
+//                Collection<String> columnNames = new LinkedList<>();
+//                columnNames.add(map.get("COLUMN_NAME"));
+//                indexMetadata = new IndexMetadata(map.get("INDEX_NAME"), schema, map.get("TABLE_NAME"),
+//                        map.get("INDEX_NAME"), !(new Boolean(map.get("NON_UNIQUE"))), columnNames);
+//                tableMetadata.registerIndex(indexMetadata);
+//            } else {
+//                Collection<String> columnNames = indexMetadata.getColumnNames();
+//                columnNames.add(map.get("COLUMN_NAME"));
+//            }
+//        }
+        return tableMap.values();
+    }
+
+    /**
+     * get index info by DatabaseMetaData.
+     * @param connection {@link Connection}
+     * @param schema {@link String}
+     * @param table {@link String}
+     * @return Map
+     * @throws SQLException notNull
+     */
+    public static Map<String, IndexMetadata> getIndexInfo(final Connection connection, final String schema, final String table) throws SQLException {
+        Map<String, IndexMetadata> indexMap = new HashMap<>(16);
+        DatabaseMetaData databaseMetaData = getDatabaseMetaData(connection);
+        List<Map<String, String>> indexInfoList = initIndex(databaseMetaData, null, schema, table, false, false);
+        for (Map<String, String> map : indexInfoList) {
+            IndexMetadata indexMetadata = indexMap.get(map.get("INDEX_NAME"));
+            if (null == indexMetadata) {
+                Collection<String> columnNames = new LinkedList<>();
+                columnNames.add(map.get("COLUMN_NAME"));
+                indexMetadata = new IndexMetadata(map.get("INDEX_NAME"), schema, map.get("TABLE_NAME"),
+                        map.get("INDEX_NAME"), !(new Boolean(map.get("NON_UNIQUE"))), columnNames);
+            } else {
+                Collection<String> columnNames = indexMetadata.getColumnNames();
+                columnNames.add(map.get("COLUMN_NAME"));
+            }
+        }
+        return indexMap;
+    }
+
+    /**
+     * get DatabaseMetaData.
+     * @param connection {@link Connection}
+     * @return DatabaseMetaData
+     * @throws SQLException notNull
+     */
+    public static DatabaseMetaData getDatabaseMetaData(final Connection connection) throws SQLException {
+        return connection.getMetaData();
+    }
+
+    /**
+     * init tables by databaseMetaData.getTables.
+     * @param databaseMetaData {@link DatabaseMetaData}
+     * @param catalog {@link String}
+     * @param schemaPattern {@link String}
+     * @param tableNamePattern {@link String}
+     * @param types {@link String}
+     * @return List
+     * @throws SQLException notNull
+     */
+    public static List<Map<String, String>> initTables(final DatabaseMetaData databaseMetaData,
+                                                       final String catalog,
+                                                       final String schemaPattern,
+                                                       final String tableNamePattern,
+                                                       final String[] types) throws SQLException {
+        ResultSet tables = databaseMetaData.getTables(catalog, schemaPattern, tableNamePattern, types);
+        List<Map<String, String>> tableList = new LinkedList<>();
+        while (tables.next()) {
+            Map<String, String> rsValue = new HashMap<>(16);
+            rsValue.put("TABLE_CAT", tables.getString("TABLE_CAT"));
+            rsValue.put("TABLE_SCHEM", tables.getString("TABLE_SCHEM"));
+            rsValue.put("TABLE_NAME", tables.getString("TABLE_NAME"));
+            rsValue.put("TABLE_TYPE", tables.getString("TABLE_TYPE"));
+            rsValue.put("REMARKS", tables.getString("REMARKS"));
+            rsValue.put("TYPE_SCHEM", tables.getString("TYPE_SCHEM"));
+            rsValue.put("TYPE_NAME", tables.getString("TYPE_NAME"));
+            rsValue.put("SELF_REFERENCING_COL_NAME", tables.getString("SELF_REFERENCING_COL_NAME"));
+            rsValue.put("REF_GENERATION", tables.getString("REF_GENERATION"));
+            tableList.add(rsValue);
+        }
+        return tableList;
+    }
+
+    /**
+     * init columns by databaseMetaData.getColumns.
+     * @param databaseMetaData {@link DatabaseMetaData}
+     * @param catalog {@link String}
+     * @param schemaPattern {@link String}
+     * @param tableNamePattern {@link String}
+     * @param columnNamePattern {@link String}
+     * @return List
+     * @throws SQLException notNull
+     */
+    public static List<Map<String, String>> initColumns(final DatabaseMetaData databaseMetaData,
+                                                        final String catalog,
+                                                        final String schemaPattern,
+                                                        final String tableNamePattern,
+                                                        final String columnNamePattern) throws SQLException {
+        ResultSet columns = databaseMetaData.getColumns(catalog, schemaPattern, tableNamePattern, columnNamePattern);
+        List<Map<String, String>> columnsList = new LinkedList<>();
+        while (columns.next()) {
+            Map<String, String> rsValue = new HashMap<>(16);
+            rsValue.put("TABLE_CAT", columns.getString("TABLE_CAT"));
+            rsValue.put("TABLE_SCHEM", columns.getString("TABLE_SCHEM"));
+            rsValue.put("TABLE_NAME", columns.getString("TABLE_NAME"));
+            rsValue.put("COLUMN_NAME", columns.getString("COLUMN_NAME"));
+            rsValue.put("TYPE_NAME", columns.getString("TYPE_NAME"));
+            rsValue.put("COLUMN_SIZE", columns.getString("COLUMN_SIZE"));
+            rsValue.put("REMARKS", columns.getString("REMARKS"));
+            rsValue.put("NULLABLE", columns.getString("NULLABLE"));
+            rsValue.put("COLUMN_DEF", columns.getString("COLUMN_DEF"));
+            columnsList.add(rsValue);
+        }
+        return columnsList;
+    }
+
+    /**
+     * init index by databaseMetaData.getIndexInfo.
+     * @param databaseMetaData {@link DatabaseMetaData}
+     * @param catalog {@link String}
+     * @param schema {@link String}
+     * @param table {@link String}
+     * @param unique {@link boolean}
+     * @param approximate {@link boolean}
+     * @return List
+     * @throws SQLException notNull
+     */
+    public static List<Map<String, String>> initIndex(final DatabaseMetaData databaseMetaData,
+                                                      final String catalog,
+                                                      final String schema,
+                                                      final String table,
+                                                      final boolean unique,
+                                                      final boolean approximate) throws SQLException {
+        ResultSet indexInfo = databaseMetaData.getIndexInfo(catalog, schema, table, unique, approximate);
+        List<Map<String, String>> indexInfoList = new LinkedList<>();
+        while (indexInfo.next()) {
+            Map<String, String> rsValue = new HashMap<>(16);
+            rsValue.put("TABLE_CAT", indexInfo.getString("TABLE_CAT"));
+            rsValue.put("TABLE_SCHEM", indexInfo.getString("TABLE_SCHEM"));
+            rsValue.put("TABLE_NAME", indexInfo.getString("TABLE_NAME"));
+            rsValue.put("INDEX_NAME", indexInfo.getString("INDEX_NAME"));
+            rsValue.put("COLUMN_NAME", indexInfo.getString("COLUMN_NAME"));
+            rsValue.put("NON_UNIQUE", indexInfo.getString("NON_UNIQUE"));
+        }
+        return indexInfoList;
+    }
+
+}
