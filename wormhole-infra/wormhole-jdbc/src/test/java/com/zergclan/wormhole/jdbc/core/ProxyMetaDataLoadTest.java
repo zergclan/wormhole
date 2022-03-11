cc34f722a9f549e621637669c685ef2a40bdad1a
wenguozhang <1138254567@qq.com>
2022-03-11 15:00:37 +0800

    get DatabaseMetaData impl

--- wormhole-infra/wormhole-jdbc/src/test/java/com/zergclan/wormhole/jdbc/core/ProxyMetaDataLoadTest.java
+++ wormhole-infra/wormhole-jdbc/src/test/java/com/zergclan/wormhole/jdbc/core/ProxyMetaDataLoadTest.java
@@ -16,14 +16,20 @@
  */
 
 package com.zergclan.wormhole.jdbc.core;
-import com.zergclan.wormhole.jdbc.api.MetaDataLoader;
-import org.junit.Test;
+
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import com.zergclan.wormhole.core.metadata.resource.ColumnMetadata;
+import com.zergclan.wormhole.core.metadata.resource.IndexMetadata;
+import com.zergclan.wormhole.core.metadata.resource.SchemaMetadata;
+import com.zergclan.wormhole.core.metadata.resource.TableMetadata;
+import com.zergclan.wormhole.jdbc.api.MetaDataLoader;
+import org.junit.Test;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Collection;
+import java.util.Optional;
 
 public class ProxyMetaDataLoadTest {
 
@@ -31,24 +37,92 @@ public class ProxyMetaDataLoadTest {
     public void assertLoadSchemas() throws SQLException {
         try (Connection connection = getConnection()) {
             MetaDataLoader metaDataLoader = new ProxyMetaDataLoad(connection);
-            metaDataLoader.loadSchemas();
+            Collection<SchemaMetadata> collection = metaDataLoader.loadSchemas();
+
+            System.out.println("schemas:");
+            for (SchemaMetadata schemaMetadata : collection) {
+                System.out.println(schemaMetadata.getName());
+            }
         }
+
     }
 
     @Test
     public void assertLoadTables() throws SQLException {
         try (Connection connection = getConnection()) {
             MetaDataLoader metaDataLoader = new ProxyMetaDataLoad(connection);
-            metaDataLoader.loadTables("test");
+            Collection<TableMetadata> collection = metaDataLoader.loadTables("slzx");
+
+            System.out.println("tables:");
+            for (TableMetadata tableMetadata : collection) {
+                System.out.println(tableMetadata.getName());
+            }
+        }
+    }
+
+    @Test
+    public void assertLoadViews() throws SQLException {
+        try (Connection connection = getConnection()) {
+            MetaDataLoader metaDataLoader = new ProxyMetaDataLoad(connection);
+            Collection<TableMetadata> collection = metaDataLoader.loadViews("slzx");
+
+            System.out.println("views:");
+            for (TableMetadata tableMetadata : collection) {
+                System.out.println(tableMetadata.getName());
+            }
+        }
+    }
+
+    @Test
+    public void assertLoadColumns() throws SQLException {
+        try (Connection connection = getConnection()) {
+            MetaDataLoader metaDataLoader = new ProxyMetaDataLoad(connection);
+            Collection<ColumnMetadata> collection = metaDataLoader.loadColumns("slzx", "source_class");
+
+            System.out.println("tables:");
+            for (ColumnMetadata columnMetadata : collection) {
+                System.out.println(columnMetadata.getName());
+            }
+        }
+    }
+
+    @Test
+    public void assertGetPrimaryKeys() throws SQLException {
+        try (Connection connection = getConnection()) {
+            MetaDataLoader metaDataLoader = new ProxyMetaDataLoad(connection);
+            Optional<IndexMetadata> optional = metaDataLoader.getPrimaryKeys("slzx", "source_class");
+
+            System.out.println("PrimaryKey:");
+            if (optional.isPresent()) {
+                System.out.println(optional.get().getName());
+            }
+        }
+    }
+
+    @Test
+    public void assertLoadIndexes() throws SQLException {
+        try (Connection connection = getConnection()) {
+            MetaDataLoader metaDataLoader = new ProxyMetaDataLoad(connection);
+            Collection<IndexMetadata> collection = metaDataLoader.loadIndexes("slzx", "source_class");
+
+            System.out.println("index:");
+            for (IndexMetadata columnMetadata : collection) {
+                System.out.println(columnMetadata.getName());
+            }
         }
     }
 
     private Connection getConnection() throws SQLException {
         HikariConfig config = new HikariConfig();
         config.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        config.setJdbcUrl("jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone=Asia/Shanghai");
-        config.setUsername("root");
-        config.setPassword("123456");
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/slzx?useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone=Asia/Shanghai");
+        config.setUsername("slzx");
+        config.setPassword("slzx");
+
+//        config.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+//        config.setJdbcUrl("jdbc:oracle:thin:@192.168.0.145:1522:shanglian");
+//        config.setUsername("shangliantest");
+//        config.setPassword("klfh0923147IHlfh");
         return (new HikariDataSource(config)).getConnection();
     }
 }
